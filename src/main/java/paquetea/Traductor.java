cc022c136e3eb905e4b66c0dd07d7037daca8a2a
ERICKBOWSER <guerig.eri@gmail.com>
2023-01-30 14:39:00 +0100

    Subida desde clase

---
+++ src/main/java/paquetea/Traductor.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquetea;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author guerig
+ */
+public class Traductor implements Comparable<Traductor>{
+    private String key;
+    private String value;
+
+    public Traductor(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.key);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Traductor other = (Traductor) obj;
+        return Objects.equals(this.key, other.key);
+    }
+
+    @Override
+    public String toString() {
+        return "Traductor{" + "key=" + key + ", value=" + value + '}';
+    }
+
+    @Override
+    public int compareTo(Traductor o) {
+        return this.key.compareToIgnoreCase(o.key);
+    }
+    
+    
+    
+    
+    
+}
